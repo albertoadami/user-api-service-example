819650927cb6943fb864a2de218440f00fbf23f0
Alberto Adami <alberto.adami.7@gmail.com>
2020-04-26 17:34:42 +0200

    little fix

--- service/src/main/scala/it/adami/api/user/UserApiMain.scala
+++ service/src/main/scala/it/adami/api/user/UserApiMain.scala
@@ -10,20 +10,22 @@ import org.http4s.dsl.io._
 import org.http4s.implicits._
 import org.http4s.server.blaze._
 
-object UserApiMain extends IOApp{
+object UserApiMain extends IOApp {
 
-  val healthService = HttpRoutes.of[IO] {
-    case GET -> Root / "health" =>
-      NoContent()
-  }.orNotFound
+  val healthService = HttpRoutes
+    .of[IO] {
+      case GET -> Root / "health" =>
+        NoContent()
+    }
+    .orNotFound
 
-    def run(args: List[String]): IO[ExitCode] =
-      BlazeServerBuilder[IO]
-        .bindHttp(8080, "0.0.0.0")
-        .withHttpApp(healthService)
-        .serve
-        .compile
-        .drain
-        .as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "0.0.0.0")
+      .withHttpApp(healthService)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 
 }
